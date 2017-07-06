e2f11a0b572f666166b1d28e7e73bb8ad4c99036
Ubuntu <ubuntu@ip-172-26-80-118.ec2.internal>
2017-07-06 20:38:20 +0000

    uncommenting RDS terraform output now that we have permisssions

--- install-pcf/aws/terraform/outputs.tf
+++ install-pcf/aws/terraform/outputs.tf
@@ -166,16 +166,15 @@ output "services_subnet_id_az3" {
 
 # RDS info
 
-# Commented out until we get rds permissions 
-#output "db_host" {
-#    value = "${aws_db_instance.pcf_rds.address}"
-#}
-#output "db_port" {
-#    value = "${aws_db_instance.pcf_rds.port}"
-#}
-#output "db_username" {
-#    value = "${aws_db_instance.pcf_rds.username}"
-#}
-#output "db_database" {
-#    value = "${aws_db_instance.pcf_rds.name}"
-#}
+output "db_host" {
+    value = "${aws_db_instance.pcf_rds.address}"
+}
+output "db_port" {
+    value = "${aws_db_instance.pcf_rds.port}"
+}
+output "db_username" {
+    value = "${aws_db_instance.pcf_rds.username}"
+}
+output "db_database" {
+    value = "${aws_db_instance.pcf_rds.name}"
+}
