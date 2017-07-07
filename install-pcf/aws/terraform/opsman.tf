a8384e244f38476d01db0478ba6ab0e09361442c
Ubuntu <ubuntu@ip-172-26-80-118.ec2.internal>
2017-07-07 15:34:37 +0000

    disabling auto-assigning a pulic ip to the OpsMan VM

--- install-pcf/aws/terraform/opsman.tf
+++ install-pcf/aws/terraform/opsman.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "opsmman_az1" {
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.directorSG.id}"]
     subnet_id = "${var.PcfVpcAccessSubnet_az1}"
-    associate_public_ip_address = true
+    associate_public_ip_address = false 
     private_ip = "${var.opsman_ip_az1}"
     root_block_device {
         volume_size = 100
