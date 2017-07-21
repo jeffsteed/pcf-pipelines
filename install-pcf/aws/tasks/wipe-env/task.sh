2f2df85e1ee614a8d6667a927e61e8d120001ce8
Ubuntu <ubuntu@ip-172-26-80-118.ec2.internal>
2017-07-21 21:10:53 +0000

    temporarily modify wipe env to ignore deleting PCF

--- install-pcf/aws/tasks/wipe-env/task.sh
+++ install-pcf/aws/tasks/wipe-env/task.sh
@@ -8,14 +8,14 @@ cd pcf-pipelines/install-pcf/aws/terraform
 source "${root}/pcf-pipelines/functions/check_opsman_available.sh"
 
 opsman_available=$(check_opsman_available $OPSMAN_URI)
-if [[ $opsman_available == "available" ]]; then
-  om-linux \
-    --target "https://${OPSMAN_URI}" \
-    --skip-ssl-validation \
-    --username $OPSMAN_USERNAME \
-    --password $OPSMAN_PASSWORD \
-    delete-installation
-fi
+#if [[ $opsman_available == "available" ]]; then
+#  om-linux \
+#    --target "https://${OPSMAN_URI}" \
+#    --skip-ssl-validation \
+#    --username $OPSMAN_USERNAME \
+#    --password $OPSMAN_PASSWORD \
+#    delete-installation
+#fi
 
 export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key}
 export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_key}
