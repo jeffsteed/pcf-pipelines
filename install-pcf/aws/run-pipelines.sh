dc1bcfe494f5801249141aecd34297287afd6096
EC2 Default User <jstff@arity.com>
2017-07-24 15:52:10 +0000

    add fly target variable to run-pipelines script and update vpc id to params file

--- install-pcf/aws/run-pipelines.sh
+++ install-pcf/aws/run-pipelines.sh
@@ -3,6 +3,7 @@
 # Run from current directory
 ENVIRONMENT_NAME=$1
 TEMP_DIR=./temp-${ENVIRONMENT_NAME}
+CONCOURSE_TARGET=pcfops
 
 if [[ ! $ENVIRONMENT_NAME =~ ^(nonprod-us-east-1|nonprod-us-west-2|prod-us-east-1|prod-us-west-2|sandbox)$ ]]; then
   echo "Must be one of (nonprod-us-east-1|nonprod-us-west-2|prod-us-east-1|prod-us-west-2|sandbox) regions."
@@ -13,9 +14,9 @@ mkdir $TEMP_DIR
 
 spruce merge params-${ENVIRONMENT_NAME}.yml > $TEMP_DIR/params.yml
 
-fly -t $ENVIRONMENT_NAME set-pipeline -n -p install-pcf --config pipeline.yml --load-vars-from $TEMP_DIR/params.yml
+fly -t $CONCOURSE_TARGET set-pipeline -n -p install-pcf --config pipeline.yml --load-vars-from $TEMP_DIR/params.yml
 
 
-fly -t $ENVIRONMENT_NAME unpause-pipeline -p install-pcf
+fly -t $CONCOURSE_TARGET unpause-pipeline -p install-pcf
 
 rm -rf $TEMP_DIR
