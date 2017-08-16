d4c199b666fad9d0fa957b98d8e557b19d5c6190
Ubuntu <jstff@arity.com>
2017-08-16 16:48:18 +0000

    add upgrades dir, add region dirs for upgrade-buildpacks, and update vault values in params yml

---
+++ upgrades/upgrade-buildpacks/run-pipelines.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run from current directory
+ENVIRONMENT_NAME=$1
+TEMP_DIR=./temp-${ENVIRONMENT_NAME}
+CONCOURSE_TARGET=pcfops
+
+if [[ ! $ENVIRONMENT_NAME =~ ^(nonprod-us-east-1|nonprod-us-west-2|prod-us-east-1|prod-us-west-2|sandbox)$ ]]; then
+  echo "Must be one of (nonprod-us-east-1|nonprod-us-west-2|prod-us-east-1|prod-us-west-2|sandbox) regions."
+  exit 1
+fi
+
+mkdir $TEMP_DIR
+
+spruce merge ./${ENVIRONMENT_NAME}/params.yml > $TEMP_DIR/params.yml
+
+fly -t $CONCOURSE_TARGET set-pipeline -n -p upgrade-buildpacks-${ENVIRONMENT_NAME} --config ./${ENVIRONMENT_NAME}/pipeline.yml --load-vars-from $TEMP_DIR/params.yml
+
+
+fly -t $CONCOURSE_TARGET unpause-pipeline -p upgrade-buildpacks-${ENVIRONMENT_NAME}
+
+rm -rf $TEMP_DIR
---
+++ upgrades/upgrade-tile/run-pipelines.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run from current directory
+ENVIRONMENT_NAME=$1
+TEMP_DIR=./temp-${ENVIRONMENT_NAME}
+CONCOURSE_TARGET=pcfops
+
+if [[ ! $ENVIRONMENT_NAME =~ ^(nonprod-us-east-1|nonprod-us-west-2|prod-us-east-1|prod-us-west-2|sandbox)$ ]]; then
+  echo "Must be one of (nonprod-us-east-1|nonprod-us-west-2|prod-us-east-1|prod-us-west-2|sandbox) regions."
+  exit 1
+fi
+
+mkdir $TEMP_DIR
+
+spruce merge ./${ENVIRONMENT_NAME}/params-${ENVIRONMENT_NAME}.yml > $TEMP_DIR/params.yml
+
+fly -t $CONCOURSE_TARGET set-pipeline -n -p install-pcf-${ENVIRONMENT_NAME} --config ./${ENVIRONMENT_NAME}/pipeline.yml --load-vars-from $TEMP_DIR/params.yml
+
+
+fly -t $CONCOURSE_TARGET unpause-pipeline -p install-pcf-${ENVIRONMENT_NAME}
+
+rm -rf $TEMP_DIR
