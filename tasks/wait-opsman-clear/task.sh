da1bbb736af2187430ff85956ecdc508d36d844d
Ubuntu <jstff@arity.com>
2017-08-17 15:39:29 +0000

    correct misspelled variable in wait-opsman-clear

--- tasks/wait-opsman-clear/task.sh
+++ tasks/wait-opsman-clear/task.sh
@@ -29,7 +29,7 @@ function main() {
   while :
   do
 
-      om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADRESS}" \
+      om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
            --skip-ssl-validation \
            --username "${OPSMAN_USERNAME}" \
            --password "${OPSMAN_PASSWORD}" \
